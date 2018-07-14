73cc7f111284a9cb65af64be332db86f93ed6666
Demon Singur <demonsingur@gmail.com>
2018-07-14 14:37:51 +0000

    r5x: Do not build useless packages
    They are compiled as dependencies of ipacm
    
    Change-Id: I72076113083308e26573289923f5d3c00a8a6b44

--- device.mk
+++ device.mk
@@ -373,7 +373,6 @@ PRODUCT_PACKAGES += \
     rild \
     libjson \
     librmnetctl \
-    libxml2 \
     libprotobuf-cpp-full
 
 PRODUCT_PACKAGES += \
@@ -423,9 +422,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # Thermal
 PRODUCT_PACKAGES += \
