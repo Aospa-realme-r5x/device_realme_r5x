6522ca5185e1e6e39d7d3bce2c4cf8f3f99c6cc7
Bruno Martins <bgcngm@gmail.com>
2022-03-14 11:56:47 +0100

    r5x: Unset BOARD_VNDK_VERSION
    PRODUCT_USE_VNDK will be true in Android-P or later launching devices
    and consequently BOARD_VNDK_VERSION will be set to 'current'.
    
    Change-Id: Iaed3eb4875c00742903627d63b4f2968e356781b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,7 +185,6 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_VENDOR_MOVE_ENABLED := true
-BOARD_VNDK_VERSION := current
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
