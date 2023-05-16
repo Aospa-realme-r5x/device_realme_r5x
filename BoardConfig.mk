17c4801a314319048a05850daf9ec9412fe5946a
Jake Weinstein <jake@aospa.co>
2023-05-16 14:01:16 +0000

    r5x: Use common compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,7 +116,6 @@ LOC_HIDL_VERSION := 3.0
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
-DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 
